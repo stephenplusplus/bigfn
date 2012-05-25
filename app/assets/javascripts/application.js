523da23e52b1a0b35e51aa14e0275a041387be3c
Stephen Sawchuk <spsawchuk@gmail.com>
2012-05-25 12:43:49 -0300

    nevermind.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,13 +2,6 @@
   //= require stretch
 
   ~ ( function ( $ ) {
-    
-    window.addEventListener( 'deviceorientation', function ( e ) {
-      var a = e.alpha;
-      var b = e.beta;
-      var g = e.gamma;
-      alert('changed?');
-    }, false);
 
     $(document).ready( function ( ) {
 
