1847723e0720f263045e9a05aa541728d064a036
Stephen Sawchuk <spsawchuk@gmail.com>
2012-05-25 12:42:02 -0300

    trying macbook orientation switch.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,13 @@
   //= require stretch
 
   ~ ( function ( $ ) {
+    
+    window.addEventListener( 'deviceorientation', function ( e ) {
+      var a = e.alpha;
+      var b = e.beta;
+      var g = e.gamma;
+      alert('changed?');
+    }, false);
 
     $(document).ready( function ( ) {
 
